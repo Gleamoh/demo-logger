6e8d44ab5c442422ccbce5f1baecc2d95f35cd22
ketwenty <ketwenty@KM-YOGA14.localdomain>
2018-03-13 11:29:35 +0100

    logger FILE

--- src/main/java/dev/console/App.java
+++ src/main/java/dev/console/App.java
@@ -1,13 +1,17 @@
+package dev.console;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;	
 
 public class App {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 		
 	public static void main(String[] args) {
-		LOGGER.info("Bonjour sdsdf");
+		LOGGER.info("Bonjour sdsdf");	
 		LOGGER.info("Imskdfsdf");
+		new AppService().executer("valeur 1");
 	}
 
 }
