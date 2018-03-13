aaecbc3b79ae82a4c1c837b72fd68217ccb735ac
ketwenty <ketwenty@KM-YOGA14.localdomain>
2018-03-13 11:02:44 +0100

    init

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+		
+	public static void main(String[] args) {
+		LOGGER.info("Bonjour sdsdf");
+		LOGGER.info("Imskdfsdf");
+	}
+
+}
